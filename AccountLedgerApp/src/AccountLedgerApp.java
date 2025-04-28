8a67f1ae75bd8cb32d2dd77d9ac04988c13c38d7
Linda Orozco <lorozco015@gmail.com>
2025-04-28 09:21:54 -0700

    Created first screen of ledger menu'Display all entries', created a switch statement, created a List with Transactions from file, created a try and catch method with the List of user entries. Created a BufferReader to read transaction inputs. Created a display transaction screen from the ledger menu and a date and time formatter.

--- AccountLedgerApp/src/AccountLedgerApp.java
+++ AccountLedgerApp/src/AccountLedgerApp.java
@@ -1,10 +1,13 @@
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AccountLedgerApp {
@@ -12,16 +15,18 @@ public class AccountLedgerApp {
     // Import Scanner in the class using static
     static Scanner scanner = new Scanner(System.in);
     static String transactionFileName = "src/transaction.csv"; // inputting here so it can be accessible
-    static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+    static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
     static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     // Where I store the methods
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         homeMenu();
-
+        ledgerMenu();
 
 
     }
+
     /// //////////////////////////////////////////////////////////////////////////////////////
 
     //The home screen should give the user the following options.  The
@@ -33,56 +38,68 @@ public class AccountLedgerApp {
     //§ L) Ledger - display the ledger screen
     //§ X) Exit - exit the application
 
-    // Create Home Menu method with prompts
-    public static void homeMenu(){
+    // Create HOME MENU  method with prompts
+    public static void homeMenu() {
         // Welcome the user
-        System.out.println("*****  Welcome to your Account Ledger Application ***** ");
+        System.out.println("\n      *****  Welcome to your Account Ledger Application *****       \n");
 
-        // How the application will continue to run until the user decided to exit
+        // How the application will continue to run until the user decides to exit
         boolean homeMenuRunning = true;
 
         /* Create while loop to repeat a block of code until the condition is met */
         while (homeMenuRunning) {
             // Ask the user what they'd like to do/ Give prompts
             System.out.println(""" 
-                ------------  What would you like to do today? ------------
-                D. Add Deposit
-                P. Make Payment
-                L. Display Ledger Screen
-                X. Exit"""); //
+                    \n------------  What would you like to do today? ------------\n
+                    D. Add Deposit
+                    P. Make Payment
+                    L. Display Ledger Screen
+                    X. Exit""");
+
             // How the user makes their selection
             String userInput = scanner.nextLine().trim().toUpperCase();
+
             // Create switch statement to allow user to choose between options
-            switch (userInput){
+            switch (userInput) {
                 case "D":
                     // Writing what is in the addDeposit() to the file created
                     Transaction newDeposit = addDeposit();
                     writeToFile(transactionFileName, newDeposit);
                     break;
                 case "P":
+                    Transaction newPayment = makePayment();
+                    writeToFile(transactionFileName, newPayment);
+                    break;
+                case "L":
+
                 case "X":
                     homeMenuRunning = false; //case running = false to get the program to stop running
                     break;
+                default:
+                    System.out.println("Invalid entry. Please select from the following options: 'D','P','L','X' ");
 
 
             }
-
-
         }
     }
+
     // addDeposit screen
     public static Transaction addDeposit() {
         // Create primitives after user input
 
         LocalDate ld = LocalDate.now();
         LocalTime lt = LocalTime.now();
+
         System.out.println("Please provide the amount of deposit");
         double amount = Double.parseDouble(scanner.nextLine());
+
         System.out.println("Please enter the vendor name: ");
         String vendor = scanner.nextLine().trim();
+
         System.out.println("Please provide a description: ");
         String description = scanner.nextLine().trim();
-        System.out.println( "Your information has been deposited! ");
+
+        System.out.println("Your information has been deposited! ");
         String idOfTransaction = "D";
 
         // Call constructor out
@@ -90,30 +107,133 @@ public class AccountLedgerApp {
         return new Transaction(ld, lt, description, idOfTransaction, vendor, amount);
     }
 
-        // Create a file method to write
-        // In () add the String variable, Class and object
-    public static void writeToFile(String fileName, Transaction transaction){
-                                                    // allows to add more to the file w.o losing what was inputted before
+    // Create a file method to write
+    // In () add the String variable, Class and object
+    public static void writeToFile(String fileName, Transaction transaction) {
+        // allows to add more to the file w.o losing what was inputted before
         try (FileWriter fw = new FileWriter(fileName, true)) {
-            fw.write(transaction.toString()+ "\n");
-        } catch (IOException e){
-            System.out.println("Something went wrong "+ e.getMessage()); // this is incase something goes wrong
+            fw.write(transaction.toString() + "\n");
+        } catch (IOException e) {
+            System.out.println("Something went wrong " + e.getMessage()); // this is incase something goes wrong
+
+        }
+
+
+    }
+
+    // make Payment screen
+    public static Transaction makePayment() {
+
+        LocalDate ld = LocalDate.now();
+        LocalTime lt = LocalTime.now();
+
+        System.out.println("Please enter the payment amount: ");
+        double amount = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Please enter the vendor name: ");
+        String vendor = scanner.nextLine().trim();
+
+        System.out.println("Please provide a description: ");
+        String description = scanner.nextLine().trim();
+
+        System.out.println("Your information has been deposited! ");
+        String idOfTransaction = "P";
+
+        return new Transaction(ld, lt, description, idOfTransaction, vendor, amount);
+
+    }
+    //• Ledger - All entries should show the newest entries first
+    //o A) All - Display all entries
+    //o D) Deposits - Display only the entries that are deposits into the
+    //account
+    //o P) Payments - Display only the negative entries (or payments)
+    //o R) Reports - A new screen that allows the user to run pre-defined
+    //reports or to run a custom search
+    //§ 1) Month To Date
+    //§ 2) Previous Month
+    //§ 3) Year To Date
+    //§ 4) Previous Year
+    //§ 5) Search by Vendor - prompt the user for the vendor name
+    //and display all entries for that vendor
+    //§ 0) Back - go back to the report page
+
+    //  Create LEDGER MENU with methods
+    public static void ledgerMenu() {
+        // Welcome the user
+        System.out.println("\n                     *****  Ledger Menu *****      \n");
+
+        // How the ledger screen will continue to run until the user decides to exit
+        boolean ledgerMenuRunning = true;
+
+        /* Create while loop to repeat a block of code until the condition is met */
+        while (ledgerMenuRunning) {
+            // Ask the user which option they'd like to pick
+
+            System.out.println(""" 
+                     \n ------------  Please select from the following options:  ------------\n
+                    A. Display all entries
+                    D. Deposits
+                    P. Payments
+                    R. Reports
+                    H. Home
+                    """);
+
+            // How the user makes their selection
+            String userInputLedger = scanner.nextLine().trim().toUpperCase();
+
+            // Create switch statement to allow user to make a choice
+            switch (userInputLedger) {
+                case "A":
+                    List<Transaction> transactions = getTransactionsFromFile(transactionFileName);
+                    displayTransaction(transactions);
+                    break;
+                case "H":
+                    ledgerMenuRunning = false;
+                    break;
+
 
             }
 
 
-    }
-    // make Payment screen
-    public static void makePayment(){
+        }
 
     }
+    public static List<Transaction> getTransactionsFromFile (String fileName){
+        // Create a list to store all transactions from user entries
+        List<Transaction> transactions = new ArrayList<>();
+
+        // Create a BufferedReader for transactions
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] arrayTransactions = line.split("\\|");
 
 
+                Transaction transaction = new Transaction(LocalDate.parse(arrayTransactions[0], dateFormatter), LocalTime.parse(arrayTransactions[1], timeFormatter), arrayTransactions[2], arrayTransactions[3], arrayTransactions[4], Double.parseDouble(arrayTransactions[5]));
 
+                // Start adding into transactions list
+                transactions.add(transaction);
+            }
 
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return transactions;
+    }
+    // Displaying a list
+    public static void displayTransaction (List<Transaction> transactions){
+        for (Transaction transaction: transactions){
+            // printing the list in a certain format %s for strings,%.2f%$%n for two numbers after '.'
+            System.out.printf("%s | %s | %s | %s | %s | %.2f%n", transaction.getDate().format(dateFormatter), transaction.getTime().format(timeFormatter), transaction.getDescription(), transaction.getIdOfTransaction(), transaction.getVendor(),transaction.getAmount());
 
-
-
-
+        }
+    }
 
 }
+
+
+
+
+
+
+
